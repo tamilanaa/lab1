bcea290be1b59d7fea36987211fa13e2627f181a
tamilana <tamilanakkali@gmail.com>
2025-02-03 01:05:21 +0500

    added second

--- l3/c.py
+++ l3/c.py
@@ -14,4 +14,22 @@ a = asd()
 a.getString()
 a.printString()
 
+#Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. 
+# Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+      
+class Shape():
+    def __init__(self, length):
+        self.length = length
+    def area(self):
+        print(0)
+
+class Square(Shape):
+    def area(self):
+        print(self.length * self.length)
+
+a = int(input())
+b = Shape(a)
+b.area()
+b = Square(a)
+b.area()
       
