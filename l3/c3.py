0644658575e0ebebe8065c3c0933963d4e485fb6
tamilana <tamilanakkali@gmail.com>
2025-02-03 14:10:28 +0500

    last

--- l3/c3.py
+++ l3/c3.py
@@ -20,3 +20,20 @@ class Account():
 wallet = Account("Sabina", 50000)
 wallet.deposit(5000)
 wallet.withdraw(100000)
+
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+class List():
+    def __init__(self, a):
+        self.a = a
+    def filtering(self):
+        return list(filter(lambda num : isPrime(num), self.a))
+
+myList = List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+print(myList.filtering())
